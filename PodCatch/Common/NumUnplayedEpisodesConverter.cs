c83593c05390bb8d3d39bfd43f38261bc34a9e38
Yoavf_cp <SND\Yoavf_cp>
2014-08-24 10:31:27 +0000

    Show more than 9 unherad episodes as 9+

---
+++ PodCatch/Common/NumUnplayedEpisodesConverter.cs
@@ -0,0 +1,30 @@
+﻿using PodCatch.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace PodCatch.Common
+{
+    public class NumUnplayedEpisodesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int numEpisodes = (int)value;
+            if (numEpisodes > 9)
+            {
+                return "9+";
+            }
+            return numEpisodes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
